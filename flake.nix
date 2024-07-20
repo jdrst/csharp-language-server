159a1c957c465d62ff4111c3e5839633074ac3dc
Sofus Addington <sofus@addington.dk>
2024-07-20 15:32:38 +0200

    add flake

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "A flake for roslyn-language-server";
+
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        roslyn-language-server = pkgs.rustPlatform.buildRustPackage {
+          pname = "roslyn-language-server";
+          version = "0.1.0";
+
+          src = ./.;
+
+          cargoLock = {
+            lockFile = ./Cargo.lock;
+          };
+        };
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = [ roslyn-language-server ];
+        };
+
+        packages.roslyn-language-server = roslyn-language-server;
+      }
+    );
+}
