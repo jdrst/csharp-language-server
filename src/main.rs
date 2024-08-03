30c4f237d38edfad76c72f69d520505e025643b1
Sofus Addington <sofus@addington.dk>
2024-08-03 19:02:12 +0200

    Force pull diagnostic capability

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use ::futures::future::try_join;
 use anyhow::{Context, Result};
 use home::home_dir;
 use rust_search::SearchBuilder;
-use serde_json::Value;
+use serde_json::{json, Value};
 use std::process::Stdio;
 use tokio::{
     io::{self, AsyncBufReadExt, AsyncReadExt, AsyncWriteExt, BufReader},
@@ -20,7 +20,9 @@ struct RoslynResponse {
 
 #[tokio::main]
 async fn main() {
-    let mut log_dir = home_dir().unwrap().into_os_string();
+    let mut log_dir = home_dir()
+        .expect("Unable to find home directory")
+        .into_os_string();
     log_dir.push("/.roslyn/logs");
 
     let mut process = Command::new("Microsoft.CodeAnalysis.LanguageServer")
@@ -40,12 +42,46 @@ async fn main() {
         .await
         .expect("Unable to connect to server stream");
 
-    let (mut reader, mut writer) = stream.split();
+    let (reader, mut writer) = stream.split();
 
     let stdin = io::stdin();
     let mut stdout = io::stdout();
 
-    let stream_to_stdout = io::copy(&mut reader, &mut stdout);
+    let stream_to_stdout = async {
+        let mut reader = BufReader::new(reader);
+        loop {
+            let mut buffer = vec![0; 3048];
+            let bytes_read = reader
+                .read(&mut buffer)
+                .await
+                .expect("Unable to read incoming server notification");
+
+            if bytes_read == 0 {
+                break; // EOF reached
+            }
+
+            let notification = String::from_utf8(buffer[..bytes_read].to_vec())
+                .expect("Unable to convert buffer to string");
+
+            if notification.contains("capabilities") {
+                let patched_result_notification = force_pull_diagnostics_hack(&notification)?;
+
+                stdout
+                    .write_all(patched_result_notification.as_bytes())
+                    .await?;
+
+                break;
+            }
+
+            stdout
+                .write_all(&buffer[..bytes_read])
+                .await
+                .expect("Unable to forward client notification to server");
+        }
+
+        io::copy(&mut reader, &mut stdout).await
+    };
+
     let stdin_to_stream = async {
         let mut stdin = BufReader::new(stdin);
         loop {
@@ -54,6 +90,7 @@ async fn main() {
                 .read(&mut buffer)
                 .await
                 .expect("Unable to read incoming client notification");
+
             if bytes_read == 0 {
                 break; // EOF reached
             }
@@ -63,11 +100,11 @@ async fn main() {
                 .await
                 .expect("Unable to forward client notification to server");
 
-            let message = String::from_utf8(buffer[..bytes_read].to_vec())
+            let notification = String::from_utf8(buffer[..bytes_read].to_vec())
                 .expect("Unable to convert buffer to string");
 
-            if message.contains("initialize") {
-                let root_path = parse_root_path(&message)
+            if notification.contains("initialize") {
+                let root_path = parse_root_path(&notification)
                     .expect("Root path not part of initialize notification");
 
                 let solution_files = find_extension(&root_path, "sln");
@@ -99,7 +136,9 @@ async fn main() {
         io::copy(&mut stdin, &mut writer).await
     };
 
-    try_join(stdin_to_stream, stream_to_stdout).await.unwrap();
+    try_join(stdin_to_stream, stream_to_stdout)
+        .await
+        .expect("Will never finish");
 }
 
 fn parse_root_path(notification: &str) -> Result<String> {
@@ -174,6 +213,24 @@ fn create_notification(body: &str) -> String {
     let full_messsage = format!("{}{}", header, body);
 
     full_messsage
+}
+
+fn force_pull_diagnostics_hack(notification: &str) -> Result<String, std::io::Error> {
+    let json_start = notification.find('{').ok_or(std::io::Error::new(
+        std::io::ErrorKind::NotFound,
+        "No JSON start found",
+    ))?;
+    let mut parsed_notification: Value = serde_json::from_str(&notification[json_start..])?;
+
+    let diagnostic_provider = json!({
+        "interFileDependencies": true,
+        "workDoneProgress": true,
+        "workspaceDiagnostics": true
+    });
+
+    parsed_notification["result"]["capabilities"]["diagnosticProvider"] = diagnostic_provider;
+
+    Ok(create_notification(&parsed_notification.to_string()))
 }
 
 #[derive(Serialize, Debug)]
