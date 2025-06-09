65d21770c970da63f9ff4020d66a94ce258854b3
Sofus Addington <sofus@addington.dk>
2025-06-09 08:35:18 +0200

    Revert "Distribute language server with binary (#140)"

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "A flake for csharp-language-server";
+
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        csharp-language-server = pkgs.rustPlatform.buildRustPackage {
+          checkFlags = [
+            # Test is unable to persist files while testing in nix
+            "--skip=first_line_is_jsonrpc"
+          ];
+
+          pname = "csharp-language-server";
+          version = "0.5.1";
+
+          src = ./.;
+
+          cargoLock = {
+            lockFile = ./Cargo.lock;
+          };
+
+          nativeBuildInputs = [ pkgs.pkgs.dotnetCorePackages.dotnet_8.sdk ];
+
+        };
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = [ csharp-language-server ];
+        };
+
+        packages.csharp-language-server = csharp-language-server;
+      }
+    );
+}
--- src/download_server.rs
+++ src/download_server.rs
@@ -1,12 +1,17 @@
-use anyhow::Result;
+use anyhow::{Result, bail};
 use std::{
-    fs::File,
-    io::Cursor,
+    env::temp_dir,
+    fs,
+    io::Write,
     path::{Path, PathBuf},
 };
-use zip::ZipArchive;
+use tokio::process::Command;
 
-pub async fn ensure_server_is_installed(version: &str, cache_dir: &Path) -> Result<PathBuf> {
+pub async fn ensure_server_is_installed(
+    version: &str,
+    remove_old_server_versions: bool,
+    cache_dir: &Path,
+) -> Result<PathBuf> {
     let server_dir = cache_dir.join("server");
 
     let dll_version_dir = server_dir.join(version);
@@ -17,28 +22,95 @@ pub async fn ensure_server_is_installed(version: &str, cache_dir: &Path) -> Resu
         return Ok(dll_path);
     }
 
-    let language_server_zip = include_bytes!("../language-server.zip");
+    let dotnet_sdk_output = match Command::new("dotnet").arg("--list-sdks").output().await {
+        Ok(output) => String::from_utf8(output.stdout)?,
+        Err(_) => bail!("Unable to get dotnet sdk version. Is dotnet installed?"),
+    };
 
-    // extract language server
-    let reader = Cursor::new(language_server_zip);
-    let mut archive = ZipArchive::new(reader)?;
+    let dotnet_sdk_version = match dotnet_sdk_output
+        .split('\n')
+        .filter_map(|line| line.chars().next())
+        .next_back()
+    {
+        Some(version) => version,
+        None => bail!("Unable to get dotnet sdk version. No sdk installations found"),
+    };
 
-    for i in 0..archive.len() {
-        let mut file = archive.by_index(i)?;
-        let outpath = dll_version_dir.join(file.name());
+    let dotnet_sdk_version_string = match dotnet_sdk_version {
+        '5' => "net5.0",
+        '6' => "net6.0",
+        '7' => "net7.0",
+        '8' => "net8.0",
+        '9' => "net9.0",
+        _ => bail!("Unsupported dotnet sdk: {}", dotnet_sdk_version),
+    };
 
-        if file.name().ends_with('/') {
-            std::fs::create_dir_all(&outpath)?;
-        } else {
-            if let Some(p) = outpath.parent() {
-                if !p.exists() {
-                    std::fs::create_dir_all(p)?;
-                }
-            }
-            let mut outfile = File::create(&outpath)?;
-            std::io::copy(&mut file, &mut outfile)?;
-        }
-    }
+    fs_extra::dir::create_all(&server_dir, remove_old_server_versions)?;
+    fs_extra::dir::create_all(&dll_version_dir, true)?;
+
+    let temp_build_root = temp_dir().join("csharp-language-server");
+    fs_extra::dir::create(&temp_build_root, true)?;
+
+    create_csharp_project(&temp_build_root, dotnet_sdk_version_string)?;
+
+    Command::new("dotnet")
+        .arg("add")
+        .arg("package")
+        .arg("Microsoft.CodeAnalysis.LanguageServer.neutral")
+        .arg("-v")
+        .arg(version)
+        .current_dir(fs::canonicalize(temp_build_root.clone())?)
+        .output()
+        .await?;
+
+    let temp_build_dir = temp_build_root
+        .join("out")
+        .join("microsoft.codeanalysis.languageserver.neutral")
+        .join(version)
+        .join("content")
+        .join("LanguageServer")
+        .join("neutral");
+
+    let copy_options = fs_extra::dir::CopyOptions::default()
+        .overwrite(true)
+        .content_only(true);
+
+    fs_extra::dir::move_dir(&temp_build_dir, &dll_version_dir, &copy_options)?;
+    fs_extra::dir::remove(temp_build_dir)?;
 
     Ok(dll_path)
+}
+
+fn create_csharp_project(temp_dir: &Path, dotnet_sdk_version_string: &str) -> Result<()> {
+    let mut nuget_config_file = std::fs::File::create(temp_dir.join("NuGet.config"))?;
+    nuget_config_file.write_all(NUGET.as_bytes())?;
+
+    let mut csproj_file = std::fs::File::create(temp_dir.join("ServerDownload.csproj")).unwrap();
+    csproj_file.write_all(csproj_string(dotnet_sdk_version_string).as_bytes())?;
+
+    Ok(())
+}
+
+const NUGET: &str = "<?xml version=\"1.0\" encoding=\"utf-8\"?>
+<configuration>
+  <packageSources>
+    <clear />
+
+    <add key=\"vs-impl\" value=\"https://pkgs.dev.azure.com/azure-public/vside/_packaging/vs-impl/nuget/v3/index.json\" />
+
+  </packageSources>
+</configuration>
+    ";
+
+fn csproj_string(dotnet_sdk_version_string: &str) -> String {
+    format!(
+        "<Project Sdk=\"Microsoft.NET.Sdk\">
+            <PropertyGroup>
+                <RestorePackagesPath>out</RestorePackagesPath>
+                <TargetFramework>{dotnet_sdk_version_string}</TargetFramework>
+                <DisableImplicitNuGetFallbackFolder>true</DisableImplicitNuGetFallbackFolder>
+                <AutomaticallyUseReferenceAssemblyPackages>false</AutomaticallyUseReferenceAssemblyPackages>
+            </PropertyGroup>
+         </Project>"
+    )
 }
